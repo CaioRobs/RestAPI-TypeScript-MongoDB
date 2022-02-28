60efe02b82a096f3d2f23048e35816aaa53a8cf7
Caio R. Vieira <caio.robs98@gmail.com>
2022-02-28 18:53:32 -0300

    initiating express app

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import config from "config";
+
+const PORT = config.get<number>("port");
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
