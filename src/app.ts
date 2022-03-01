8dbe468a48490a0ecb1639e00427f30321b93f07
Caio R. Vieira <caio.robs98@gmail.com>
2022-02-28 21:27:00 -0300

    init mongoose connection and aplly routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import config from "config";
+import routes from './routes'
+import connect from "./utils/connection";
+import log from "./utils/logger";
 
 const PORT = config.get<number>("port");
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
+app.listen(PORT, async () => {
+  log.info(`app running at http://localhost:${PORT}`);
+
+  await connect()
+
+  routes(app)
 });
