7560efaf16411bd3378ca23280838adbf15aafbe
Caio R. Vieira <caio.robs98@gmail.com>
2022-02-28 21:32:18 -0300

    health check route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
-import {Express} from 'express'
+import { Express, Request, Response } from "express";
 
 const routes = (app: Express) => {
+  app.get("/healthcheck", (_req: Request, res: Response) =>
+    res.sendStatus(200)
+  );
+};
 
-}
-
-export default routes
+export default routes;
