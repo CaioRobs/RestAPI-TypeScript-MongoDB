30ae8c09efe6fb9f448fe7ab09ad8ecc8667032c
Caio R. Vieira <caio.robs98@gmail.com>
2022-03-17 15:58:44 -0300

    fix start script and compiling errors

--- src/schemas/sessionSchema.ts
+++ src/schemas/sessionSchema.ts
@@ -1,13 +1,11 @@
 import { object, string, TypeOf } from "zod";
 
 export const createSessionSchema = object({
-  body: object({
-    email: string({
-      required_error: "Email is required",
-    }).email("email is no valid"),
-    password: string({
-      required_error: "Password is required",
-    }),
+  email: string({
+    required_error: "Email is required",
+  }).email("email is no valid"),
+  password: string({
+    required_error: "Password is required",
   }),
 });
 
